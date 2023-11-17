b79ad3652d89926a66514f1575a7c007a96897a1
Irfan Akyavas <irfanakyavas@gmail.com>
2023-11-17 16:53:02 +0300

    arayuz

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,55 @@
 using namespace RoutingKit;
 using namespace std;
 
+unsigned userCoordInput(GeoPositionToNode* gp2n)
+{
+
+	float outputLongitude, outputLatitude;
+	unsigned pointID;
+	while(1)
+	{
+	bool success = false;
+
+	cout << "Enter latitude: ";
+	while (!success)
+	{
+		cin >> outputLatitude;
+		if(cin.fail())
+		{
+			cout << "Wrong format, was expecting float. Try again..." << endl;
+		}
+		else
+		{
+			success = true;
+			cout << endl;
+		}
+	}
+
+	success = false;
+	cout << "Enter longitude: ";
+	while (!success)
+	{
+		cin >> outputLongitude;
+		if(cin.fail())
+		{
+			cout << "Wrong format, was expecting float. Try again...\n";
+		}
+		else
+		{
+			success = true;
+			cout << endl;
+		}
+	}
+	pointID = gp2n->find_nearest_neighbor_within_radius(outputLatitude, outputLongitude, 1000).id;
+	if(pointID == invalid_id)
+		cout << "No node in reasonable distance or outside the map. Try again..." << endl;
+	else
+		return pointID;
+	}
+
+}
+
 int main(){
-	cout << "Hello" << endl;
 	// Load a car routing graph from OpenStreetMap-based data
 	auto graph = simple_load_osm_car_routing_graph_from_pbf("Turkiye.pbf");
 	auto tail = invert_inverse_vector(graph.first_out);
@@ -27,30 +74,23 @@ int main(){
 	ContractionHierarchyQuery ch_query(ch);
 
 	// Use the query object to answer queries from stdin to stdout
-	float from_latitude, from_longitude, to_latitude, to_longitude;
-	while(cin >> from_latitude >> from_longitude >> to_latitude >> to_longitude){
-		unsigned from = map_geo_position.find_nearest_neighbor_within_radius(from_latitude, from_longitude, 1000).id;
-		if(from == invalid_id){
-			cout << "No node within 1000m from source position" << endl;
-			continue;
-		}
-		unsigned to = map_geo_position.find_nearest_neighbor_within_radius(to_latitude, to_longitude, 1000).id;
-		if(to == invalid_id){
-			cout << "No node within 1000m from target position" << endl;
-			continue;
-		}
+	unsigned from, to;
 
-		long long start_time = get_micro_time();
-		ch_query.reset().add_source(from).add_target(to).run();
-		auto distance = ch_query.get_distance();
-		auto path = ch_query.get_node_path();
-		long long end_time = get_micro_time();
+	cout << "For the source point:" << endl;
+	from = userCoordInput(&map_geo_position);
+	cout << "For the destination point:" << endl;
+	to = userCoordInput(&map_geo_position);
 
-		cout << "To get from "<< from << " to "<< to << " one needs " << distance << " milliseconds." << endl;
-		cout << "This query was answered in " << (end_time - start_time) << " microseconds." << endl;
-		cout << "The path is";
-		for(auto x:path)
-			cout << " " << x;
-		cout << endl;
-	}
+	long long start_time = get_micro_time();
+	ch_query.reset().add_source(from).add_target(to).run();
+	auto distance = ch_query.get_distance();
+	auto path = ch_query.get_node_path();
+	long long end_time = get_micro_time();
+
+	cout << "To get from "<< from << " to "<< to << " one needs " << distance << " milliseconds." << endl;
+	cout << "This query was answered in " << (end_time - start_time) << " microseconds." << endl;
+	cout << "The path is";
+	for(auto x:path)
+		cout << " " << x;
+	cout << endl;
 }
